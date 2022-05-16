2bdc15b7947ae6f3f9aa8a0cfa748f63c9089479
Bannon Tanner <bannon.tanner@perficient.com>
2022-05-16 11:46:59 -0500

    Add project files.

---
+++ MusicApi/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        // constructor, provide DB connection info to
+        public ApiDbContext(DbContextOptions<ApiDbContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
